d0dafe91f29bd09df09caf33994dce5329df0948
Moisés de Souza <contatomoisesalexandre@gmail.com>
2021-12-28 22:18:28 -0300

    [PRODUTORES][FIX] ajuste layout

--- produtores/routes/web.php
+++ produtores/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('layout.index');
 });
+
+
+// Route::get('/home', 'HomeController@index')->name('home');
